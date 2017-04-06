fb875af082de0c68dab8635774b6a120ff67fad8
Byron Tardif <byvinyal@microsoft.com>
2017-04-06 15:11:42 -0700

    adding sample application

---
+++ Sample-AppService-Logging/Sample-AppService-Logging/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sample_AppService_Logging.MvcApplication" Language="C#" %>
---
+++ Sample-AppService-Logging/Sample-AppService-Logging/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sample_AppService_Logging.Startup))]
+namespace Sample_AppService_Logging
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
